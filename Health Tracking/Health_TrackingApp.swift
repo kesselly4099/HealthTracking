4b6e88f939cfec1457fd6ab547aeebc90688c980
kesselly kamara <kkesselly30@gmail.com>
2024-05-25 00:25:23 -0700

    Initial Commit

---
+++ Health Tracking/Health_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_TrackingApp.swift
+//  Health Tracking
+//
+//  Created by Kesselly Kamara on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Health_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Health TrackingUITests/Health_TrackingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Health_TrackingUITestsLaunchTests.swift
+//  Health TrackingUITests
+//
+//  Created by Kesselly Kamara on 5/25/24.
+//
+
+import XCTest
+
+final class Health_TrackingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
